0436223f54a4601fe48eb9141b7562fc51739e3d
GitHub Action <action@github.com>
2020-08-07 13:46:38 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/captainkant/myphpstormpersonal/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.captainkant.myphpstormpersonal.services
+
+import com.github.captainkant.myphpstormpersonal.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
